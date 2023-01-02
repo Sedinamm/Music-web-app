7b56fde5a9207bd3bed31b4fa5b9d68d3511481d
Sedinam <sedinam436@gmail.com>
2023-01-02 12:23:10 -0300

    made corrections in shazamCore

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,26 +1,3 @@
-// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
-//     export const shazamCoreApi = createApi({
-//         reducerPath: 'shazamCoreApi',
-//         baseQuery: fetchBaseQuery({
-//             baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
-//             prepareHeaders: (headers) => {
-//                 headers.set('X-RapidAPI-Key', 'e25969e3c6mshe532816f9b599fbp1f9729jsnf6830f0b148b' );
-
-//                 return headers;
-//             },
-//         }),
-//         endpoints: (builder) => ({
-//             getTopCharts: builder.query({query: () => '/charts/world'}),
-//             getSongDetails: builder.query({ query:({ songid }) => `/ tracks/details?track_id=${songid}` }),
-//         }),
-//     });
-
-//     export const {
-//         useGetTopChartsQuery,
-//         useGetSongDetailsQuery,
-//     } = shazamCoreApi;
-
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const shazamCoreApi = createApi({
